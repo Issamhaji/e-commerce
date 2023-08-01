cf89548c1bcdd5d1eea0a09c9f59339f546e5087
issam haji <ihaji@sqli.com>
2023-08-01 19:18:46 +0100

    integration templates & stof-foundry-fixtures

---
+++ src/Controller/CartController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    #[Route('/cart', name: 'app_cart')]
+    public function index(): Response
+    {
+        return $this->render('cart/index.html.twig', [
+            'controller_name' => 'CartController',
+        ]);
+    }
+
+    #[Route(path: '/checkout', name: 'app_checkout')]
+    public function checkout(): Response
+    {
+        return $this->render('cart/checkout.html.twig');
+    }
+}
