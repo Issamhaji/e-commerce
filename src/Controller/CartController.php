df72d060fb290c5140fb8480029f15773d6e8b8e
issam haji <ihaji@sqli.com>
2023-08-03 11:22:29 +0100

    add form and working with services

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -4,17 +4,36 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\CartItem;
+use App\Entity\Product;
+use App\Repository\CartItemRepository;
+use App\Service\CartInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+    private readonly CartInterface $cartHandler;
+
+    /**
+     * This will inject the default service CartHandler.
+     * If you want to use the CartFile, rename the argument to $cartFile. @see config/services.yaml file.
+     *
+     * @param CartInterface $handler
+     */
+    public function __construct(CartInterface $handler)
+    {
+        $this->cartHandler = $handler;
+    }
+
     #[Route('/cart', name: 'app_cart')]
     public function index(): Response
     {
+        $cart = $this->cartHandler->getCartInstance();
+
         return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+            'cart' => $cart,
         ]);
     }
 
@@ -22,5 +41,43 @@ class CartController extends AbstractController
     public function checkout(): Response
     {
         return $this->render('cart/checkout.html.twig');
+    }
+
+    #[Route('/cart/add/{id}', name: 'add_to_cart')]
+    public function addToCart(Product $product, CartItemRepository $repository): Response
+    {
+        // @TODO: check if the product exists in the cart
+
+        // @TODO: increment the quantity when the product already exists
+
+        $cart = $this->cartHandler->getCartInstance();
+        $quantity = 1;
+        $items = $cart->getCartItems();
+        $found = false;
+        foreach ($items as $cartItem) {
+            if ($cartItem->getProduct() === $product) {
+                $previousQuantity = $cartItem->getQuantity();
+                $newQuantity = $previousQuantity + $quantity;
+                $cartItem->setQuantity($newQuantity);
+                $found = true;
+                break;
+            }
+        }
+
+        // @TODO: Add the product to the cart if not exist
+        if (false === $found) {
+            $cartItem = new CartItem();
+            $cartItem->setQuantity($quantity)
+                ->setCart($cart)
+                ->setProduct($product);
+        }
+
+        // @TODO: save to database
+        $repository->save($cartItem, false === $found);
+
+        // @TODO: redirect the user to the cart page
+        $this->addFlash('success', 'Product added to cart');
+
+        return $this->redirectToRoute('app_cart');
     }
 }
