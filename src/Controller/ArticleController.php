05d4fb0cf355264e0cd332db2477959f564a837a
issam haji <ihaji@sqli.com>
2023-08-25 13:40:22 +0100

    Revert "integrate article&&category templates"
    This reverts commit 6aee955113cfc490129faea04f4ee27cfc51281c.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,22 +2,17 @@
 
 namespace App\Controller;
 
-
-use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/article/{slug}', name: 'app_article')]
-    public function index(ArticleRepository $repository, Request $request): Response
+    #[Route('/article', name: 'app_article')]
+    public function index(): Response
     {
-        $slug = $request->attributes->get('slug');
-        $article = $repository->findBy(['slug'=> $slug]);
         return $this->render('article/index.html.twig', [
-            'article' => $article,
+            'controller_name' => 'ArticleController',
         ]);
     }
 }
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,28 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
-use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-#[Route('/category')]
+
 class CategoryController extends AbstractController
 {
-    #[Route('/{slug}', name: 'app_category')]
-    public function index(Category $category): Response
+    #[Route('/categories', name: 'app_category')]
+    public function index(): Response
     {
         return $this->render('category/index.html.twig', [
             'controller_name' => 'CategoryController',
-        ]);
-    }
-
-    public function category(CategoryRepository $repository): Response
-    {
-        $categories = $repository->findAll();
-
-        return $this->render('category/_category_items.html.twig', [
-            'categories' => $categories,
         ]);
     }
 }
